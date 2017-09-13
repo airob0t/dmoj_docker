cbcf4d6e060407f01f058f8d38cc56bea993c53b
airob0t <anymouslinux@gmail.com>
2017-09-13 04:32:33 -0400

    update

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-docker run --name=dmoj_site --link=dao_dmojdb_1:dbhost -p 80:80 -t -i -d airobot/dmoj /bin/bash
+#docker run --name=dmoj_site --link=dao_dmojdb_1:dbhost -p 80:80 -t -i -d airobot/dmoj /bin/bash
+docker run --name=dmoj_site -p 80:80 -p 9999:9999 -p 9998:9998 -p 15100:15100 -p 15101:15101 -p 15102:15102 -t -i -d airobot/dmoj /bin/bash
